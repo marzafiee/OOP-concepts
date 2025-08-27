14e83a3c2ed5babd89a5833c7b45a2b3e847ffc2
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-27 22:18:20 +0000

    created abstract class BankAccount

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+/**
+ * Abstract class representing a generic bank account.
+ *
+ * Features:
+ * - Fields: accountNumber, accountHolderName, and balance.
+ * - Abstract method: calculateInterest()
+ * - Final method: displayAccountDetails() to print account information.
+ *
+ * Author: Inez Agbenu
+ * Date: August 27, 2025
+ */
+public abstract class BankAccount {
+    // instantiating variables
+}
