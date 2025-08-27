dc324fb9ab0007c0b9c0904b7a6d6906c472f22d
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-27 22:30:43 +0000

    finished exercise 1

--- BankAccount.java
+++ BankAccount.java
@@ -10,5 +10,31 @@
  * Date: August 27, 2025
  */
 public abstract class BankAccount {
-    // instantiating variables
-}
+    // instantiating variables or fields
+    private String accountHolderName;
+    private int accountNumber;
+    private double balance;
+
+    // constructor
+    public BankAccount(String accountHolderName, int accountNumber, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    // abstract method: calculateInterest()
+    public abstract double calculateInterest(double rate, int months);
+
+    //getters
+    public double getBalance() { return balance;}
+    public int getAccountNumber() { return accountNumber; }
+    public String getAccountHolderName() { return accountHolderName; }
+
+    // final method that prints account information
+    public final void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder Name: " + accountHolderName);
+        System.out.println("Current balance: " + balance);
+    }
+
+} // class
