453dc8bd9b918859752c648880dbc940fea3c4ea
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-28 08:44:33 +0000

    created interface LoanAccount

---
+++ LoanAccount.java
@@ -0,0 +1,12 @@
+/**
+ * interface
+ *
+ * Features:
+ * - Has a method applyForLoan() that SavingsAccount class implements
+ *
+ * Author: Inez Agbenu
+ * Date: August 27, 2025
+ */
+public interface LoanAccount{
+    boolean applyForLoan(); // note that this method is both public and abstract by default
+}
