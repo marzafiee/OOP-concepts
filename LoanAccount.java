8375b468618a40fd79a94b93b2ac505b5c83f9fe
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-28 09:27:18 +0000

    fixed parameter issue with overriding

--- LoanAccount.java
+++ LoanAccount.java
@@ -8,5 +8,5 @@
  * Date: August 27, 2025
  */
 public interface LoanAccount{
-    boolean applyForLoan(); // note that this method is both public and abstract by default
+    boolean applyForLoan(double loanAmount, double yearsOfLoan); // note that this method is both public and abstract by default
 }
